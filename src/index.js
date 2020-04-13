a6f817ff112f93437088450056a2ddd9b7a9d228
Artem <speker@mail.ru>
2020-04-13 19:46:13 +0400

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const appEl = document.querySelector('#app');
+appEl.innerHTML = 'Editor';
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Editor',
+            templateContent: ({htmlWebpackPlugin}) => `
+            <html>
+              <head>
+                ${htmlWebpackPlugin.tags.headTags}
+              </head>
+              <body>
+                <div id="app"></div>
+                ${htmlWebpackPlugin.tags.bodyTags}
+              </body>
+            </html>
+          `
+        })
+    ]
+}
